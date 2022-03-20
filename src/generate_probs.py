89db9914d122f8ef12a8f8c13dbdda737e1b17a3
Bryan <bryan.wang@berkeley.edu>
2022-03-20 17:07:22 -0400

    add simulating distribution

--- src/generate_probs.py
+++ src/generate_probs.py
@@ -1,15 +1,16 @@
 import pandas as pd
 import numpy as np
-from tqdm import trange
-
+from tqdm import trange, tqdm
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 LEVEL_TO_PROBS = {
     4: [.55, .3, .15, 0, 0],
     5: [.45, .33, .2, .02, 0],
     6: [.25, .4, .3, .05, 0],
     7: [.19, .3, .35, .15, .01],
-    8: [.15, .2, .35, .25, .05],
-    9: [.1, .15, .3, .3, .15]
+    8: [.16, .2, .35, .25, .04],
+    9: [.09, .15, .3, .3, .16]
 }
 
 TIERS = [1, 2, 3, 4, 5]
@@ -24,7 +25,7 @@ CHAMPIONS_PER_TIER = {
 
 
 class Simulator:
-    def __init__(self, success_criteria, level=5, copies_already_held: dict={}, champions_missing:dict ={}) -> None:
+    def __init__(self, success_criteria, level=5, copies_already_held: dict={}, champions_missing:dict ={}, semantics='and') -> None: # TODO: can consolidate copies held / champs missing.
         self.unweighted_probs = {
             1: [29]*13,
             2: [22]*13,
@@ -36,11 +37,22 @@ class Simulator:
         self.champions_missing = champions_missing
         for tier, champion in copies_already_held:
             self.unweighted_probs[tier][champion] -= copies_already_held[(tier, champion)]
+        self.remove_champions()
+        print(self.unweighted_probs)
         self.level = level
+        self.semantics = semantics
+        assert self.semantics in ['and', 'or']
 
     def remove_champions(self):
         for tier in self.champions_missing:
-            keys_of_tier = [key for key in self.success_criteria if ]
+            max_champion_idx = max([champion for t, champion in self.success_criteria if t == tier]) + 1
+            removed_count = 0
+            while removed_count < self.champions_missing[tier]:
+                if self.unweighted_probs[tier][max_champion_idx] == 0:
+                    max_champion_idx += 1
+                self.unweighted_probs[tier][max_champion_idx] -= 1
+                removed_count += 1
+
     def normalize_unweighted(self, probs):
         denom = sum(probs)
         return [prob/denom for prob in probs]
@@ -84,9 +96,14 @@ class Simulator:
 
     def is_successful(self, success_dict, success_criteria):
         successes = zip(success_dict.values(), success_criteria.values())
-        if all(current > expected for current, expected in successes):
-            return True
-        return False
+        if self.semantics == 'and':
+            if all(current > expected for current, expected in successes):
+                return True
+            return False
+        elif self.semantics == 'or':
+            if any(current > expected for current, expected in successes):
+                return True
+            return False      
 
     def simulate_once(self, success_criteria, num_gold): # dict of number of each champion desired.
         success_dict = {champion: 0 for champion in success_criteria.keys()}
@@ -105,20 +122,40 @@ class Simulator:
 
     def simulate(self, success_criteria, num_gold, n=10000):
         return sum([self.simulate_once(success_criteria, num_gold) for _ in range(n)])
+
+    def simulate_dist_once(self, success_criteria): # dict of number of each champion desired.
+        success_dict = {champion: 0 for champion in success_criteria.keys()}
+        hands = 1
+        while not self.is_successful(success_dict, success_criteria):
+            self.get_hand(success_dict)
+            # print(success_dict)
+            hands += 1
+        self.reset_probabilities()
+        return hands
+
+
+    def simulate_dist(self, success_criteria, n=10000):
+        tries = []
+        for _ in trange(n):
+            tries.append(self.simulate_dist_once(success_criteria))
+        return tries
         
 
 if __name__ == '__main__':
-    N = 10000
+    N = 100000
     probs = {}
-    for level in [4, 5]:
-        copies_already_held =  {(1, 0): 6}
-        sim = Simulator(level=level, copies_already_held=copies_already_held)
-        probs[level] = []
-        success_criteria =  {(1, 0): 3}
-        for i in trange(20, 50, 2):
-            prob = sim.simulate(success_criteria, i, n=N) / N
-            probs[level].append(prob)
-            print(f'level: {level}, {i}: {prob}')
-
-    pd.DataFrame(probs).to_csv('gold.csv', index=False)
+    for level in [7]:
+        success_criteria =  {(4, 0): 1} #, (4, 1): 3, (4, 2): 3, (4, 3): 3}
+        copies_already_held =  {(4, 0): 2}
+        champions_missing = {} # {1: 40, 2: 20, 3: 10}
+        sim = Simulator(success_criteria, level=level, copies_already_held=copies_already_held, champions_missing=champions_missing, semantics='or')
+        # probs[level] = []
+        # for i in trange(20, 70, 2):
+        #     prob = sim.simulate(success_criteria, i, n=N) / N
+        #     probs[level].append(prob)
+        #     print(f'level: {level}, {i}: {prob}')
+        res = sim.simulate_dist(success_criteria, n=N)
+        sns.displot(res, kde=True)
+        plt.savefig('dist.png')
+    # pd.DataFrame(probs).to_csv('4_carries_distribution_7_need_1.csv', index=False)
         
